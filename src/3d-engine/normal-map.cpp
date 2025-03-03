61e46775add6fee921888ebfbf8b960abd56fa4f
fasxmut <fasxmut>
2025-03-03 04:24:27 +0000

    Update

--- src/3d-engine/normal-map.cpp
+++ src/3d-engine/normal-map.cpp
@@ -1,3 +1,10 @@
+//
+// Copyright (c) 2025 Fas Xmut (fasxmut at protonmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #include <testpub/core.hpp>
 #include <filesystem>
 #include <lyra/lyra.hpp>
